c67e87c3c33b3fac1edc2d631507bcef0350bf21
Olivier Bossel <olivier.bossel@gmail.com>
2017-11-17 15:23:19 +0100

    fix extend

--- src/functions/objects/t_extend.php
+++ src/functions/objects/t_extend.php
@@ -9,9 +9,12 @@
  * @author 		Olivier Bossel <olivier.bossel@gmail.com>
  */
 function t_extend($a, $b, $deep = true) {
-	
+
 	// ensure that we have a $b parameter
 	if ( ! $b) return $a;
+
+	// print '<pre>';
+	// print_r($b);
 
 	// save if the $a item is an object
 	// to restore it as is after processing
@@ -21,8 +24,8 @@ function t_extend($a, $b, $deep = true) {
 	// merge the items
     foreach($b as $k=>$v) {
 		if( is_array($v) || is_object($v) ) {
-            if( !isset($a[$k]) ) {
-                $a[$k] = $v;
+            if( ! isset($a[$k])) {
+				$a[$k] = $v;
             } else {
 
 				$is_v_object = is_object($v);
@@ -31,15 +34,15 @@ function t_extend($a, $b, $deep = true) {
 				$a[$k] = (array) $a[$k];
 
 				if ($deep) {
-					$a[$k] = array_unique(array_merge($a[$k], t_extend($a[$k], $v, $deep)));
+					$a[$k] = array_merge($a[$k], t_extend($a[$k], $v, $deep));
 				} else {
 					$a[$k] = $v;
 				}
 
 				if ($is_ak_object) $a[$k] = (object) $a[$k];
 			}
-        } else {
-            $a[$k] = $v;
+        } else if ( ! isset($a[$k]) || ! is_null($v)) {
+			$a[$k] = $v;
         }
 	}
 
