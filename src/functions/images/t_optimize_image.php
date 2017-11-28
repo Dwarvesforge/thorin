19bb8896bebe56524feb3394fdc0d0672a549d8f
Olivier Bossel <olivier.bossel@gmail.com>
2017-11-28 11:08:31 +0100

    up

--- src/functions/images/t_optimize_image.php
+++ src/functions/images/t_optimize_image.php
@@ -1,10 +1,26 @@
 <?php
 
+// import the Intervention Image Manager Class
+use Intervention\Image\ImageManager;
+
+// create an image manager instance with favored driver
+$manager = new ImageManager(array('driver' => 'imagick'));
+
 function t_optimize_image($path) {
-	// new instance
-	$imageCache = new ImageCache();
-	// set the cache directory
-	$imageCache->cached_image_directory = t_tailslash(T_CACHE_PATH) . 'images/';
-	// cache and return the compressed image
-	return $imageCache->cache($path);
+
+
+// to finally create image instances
+	$img = Image::cache(function($image) {
+		$image = $image->make($path)->save(t_tailslash(T_CACHE_PATH) . 'images/' . basename($path));
+	});
+	print '<pre>';
+	print_r($img);
+	return $img;
+
+	// // new instance
+	// $imageCache = new ImageCache();
+	// // set the cache directory
+	// $imageCache->cached_image_directory = t_tailslash(T_CACHE_PATH) . 'images/';
+	// // cache and return the compressed image
+	// return $imageCache->cache($path);
 }
