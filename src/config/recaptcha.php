b1c52195c79b4c721cb482ea3764122356e05b0f
Olivier Bossel <olivier.bossel@gmail.com>
2018-11-09 14:17:14 +0100

    clean

--- src/config/recaptcha.php
+++ src/config/recaptcha.php
@@ -6,21 +6,21 @@ return [
 	 * The recaptcha secret
 	 * @type    {String}
 	 */
-	'secret' => Thorin::env('RECAPTCHA_SECRET') ?: 'secret',
+	'secret' => Thorin::env('RECAPTCHA_SECRET', 'secret'),
 
 	/**
 	 * @name    sitekey
 	 * The recaptcha sitekey
 	 * @type    {String}
 	 */
-	'sitekey' => Thorin::env('RECAPTCHA_SITEKEY') ?: 'sitekey',
+	'sitekey' => Thorin::env('RECAPTCHA_SITEKEY', 'sitekey'),
 
 	/**
 	 * @name    lang
 	 * The recaptcha lang to use
 	 * @type    {String}
 	 */
-	'lang' => Thorin::env('RECAPTCHA_LANG') ?: Thorin::language(),
+	'lang' => Thorin::env('RECAPTCHA_LANG', Thorin::language()),
 
 	/**
 	 * @name    attributes
@@ -34,7 +34,7 @@ return [
 		 * @type    {String}
 		 * @default    light
 		 */
-		'data-theme' => Thorin::env('RECAPTCHA_THEME') ?: 'light',
+		'data-theme' => Thorin::env('RECAPTCHA_THEME', 'light'),
 
 		/**
 		 * @name    attributes.data-type
@@ -42,7 +42,7 @@ return [
 		 * @type    {String}
 		 * @default    image
 		 */
-		'data-type' => Thorin::env('RECAPTCHA_TYPE') ?: 'image',
+		'data-type' => Thorin::env('RECAPTCHA_TYPE', 'image'),
 
 		/**
 		 * @name    attributes.data-size
@@ -50,6 +50,6 @@ return [
 		 * @type    {String}
 		 * @default    normal
 		 */
-		'data-size' => Thorin::env('RECAPTCHA_SIZE') ?: 'normal'
+		'data-size' => Thorin::env('RECAPTCHA_SIZE', 'normal')
 	]
 ];
