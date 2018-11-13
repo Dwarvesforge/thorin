aefdf52f17f338f66f442905c69bf6b798bd7c0f
Olivier Bossel <olivier.bossel@gmail.com>
2018-11-13 16:02:20 +0100

    add is_end_with function

---
+++ tests/functions/strings/t_is_end_withTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class t_is_end_with extends TestCase
+{
+    public function testSimpleText() {
+		$text = 'Hello world this is a nice sample of text.';
+		$isEndWith = Thorin::is_end_with($text, 'sample of text.');
+		$this->assertEquals($isEndWith, true);
+	}
+}
