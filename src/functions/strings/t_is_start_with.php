2773091a56bf989cdf9caabdfca24d047c7f0ff7
Olivier Bossel <olivier.bossel@gmail.com>
2018-11-13 16:02:49 +0100

    add is_start_with function

---
+++ src/functions/strings/t_is_start_with.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Check if the passed string start with the other passed string
+ * @param    {String}    $source    The source string to check
+ * @param    {String}    $with    The "with" string to check
+ * @return    {Boolean}    true if start with, false if not
+ *
+ * @example    php
+ * if (Thorin::is_start_with($myText, 'hello world')) {
+ *   // do something here
+ * }
+ *
+ * @author    Olivier Bossel <olivier.bossel@gmail.com> (https://olivierbossel.com)
+ */
+function t_is_start_with($source, $with) {
+	return strpos($source, $with) === 0;
+}
