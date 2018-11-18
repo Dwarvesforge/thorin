0466cae77cb111906a8f3b584872dff3d00386a1
Olivier Bossel <olivier.bossel@gmail.com>
2018-11-18 16:54:00 +0100

    add t_col_each_right function

---
+++ src/functions/collections/t_col_each_right.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This method is like `Thorin::col_each` except that it iterates over elements of
+ * `collection` from right to left.
+ *
+ * @param    {Array|Iterable|Object}    $collection    The collection to iterate over
+ * @param    {Callable}    $iteratee    The function invoked per iteration
+ * @return    {Array|Object}    Returns `collection`
+ *
+ * @example    php
+ * Thorin::col_each_right([1, 2], function($value) { echo $value; })
+ * // Echoes `2` then `1`
+ *
+ * @author    Olivier Bossel <olivier.bossel@gmail.com> (https://olivierbossel.com)
+ * @see    https://github.com/lodash-php/lodash-php/blob/master/src/Collection/eachRight.php
+ */
+function t_col_each_right($collection, callable $iteratee) {
+	return _::eachRight($collection, $iteratee);
+}
