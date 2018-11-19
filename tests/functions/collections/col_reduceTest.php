4c4284deaf19b4f99fad1740fe2cf718d2efe07f
Olivier Bossel <olivier.bossel@gmail.com>
2018-11-19 13:03:39 +0100

    fix col_reduce test

--- tests/functions/collections/col_reduceTest.php
+++ tests/functions/collections/col_reduceTest.php
@@ -7,11 +7,6 @@ use PHPUnit\Framework\TestCase;
 final class col_reduce extends TestCase
 {
     public function testReduce() {
-		$users = [
-		  ['user' => 'barney',  'age' => 36, 'active' => false],
-		  ['user' => 'fred',    'age' => 40, 'active' => true],
-		  ['user' => 'pebbles', 'age' => 1,  'active' => false]
-		];
 		$res = Thorin::col_reduce(['a' => 1, 'b' => 2, 'c' => 1], function ($result, $value, $key) {
 		   if (!isset($result[$value])) {
 		       $result[$value] = [];
