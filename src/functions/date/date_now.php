98da3daad5839c22043069056c346a94094a0805
Olivier Bossel <olivier.bossel@gmail.com>
2018-11-19 17:03:07 +0100

    renate now to date_now

--- src/functions/date/date_now.php
+++ src/functions/date/date_now.php
@@ -6,12 +6,12 @@ namespace thorin;
  * Gets the timestamp of the number of milliseconds that have elapsed since the Unix epoch (1 January 1970 00:00:00 UTC).
  *
  * @example    php
- * Thorin::now();
+ * Thorin::date_now();
  * // 1511180325735
  *
  * @author    Olivier Bossel <olivier.bossel@gmail.com> (https://olivierbossel.com)
  * @see    https://github.com/lodash-php/lodash-php/blob/master/src/Date/now.php
  */
-function now():int {
+function date_now():int {
 	return \_::now();
 }
